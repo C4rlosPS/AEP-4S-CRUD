f4334197f82b4dcff98218f7d71b4fe651839f6a
Carlos Eduardo <cadu.ps04@gmail.com>
2023-11-04 10:22:16 -0300

    metodo dele criado

--- OrganizadorTarefas/src/main/java/com/aep4s/organizadortarefas/Model/Controller/ControllerTarefa.java
+++ OrganizadorTarefas/src/main/java/com/aep4s/organizadortarefas/Model/Controller/ControllerTarefa.java
@@ -4,10 +4,99 @@
  */
 package com.aep4s.organizadortarefas.Model.Controller;
 
+import com.aep4s.organizadortarefas.Model.Tarefa;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Usuário
  */
 public class ControllerTarefa {
-    
+
+    private static ControllerTarefa instance;
+    protected EntityManager entityManager;
+
+    public static ControllerTarefa getInstance() {
+        if (instance == null) {
+            instance = new ControllerTarefa();
+        }
+        return instance;
+    }
+
+    private ControllerTarefa() {
+        entityManager = getEntityManager();
+    }
+
+    private EntityManager getEntityManager() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa_exemplo");
+        if (entityManager == null) {
+            entityManager = factory.createEntityManager();
+        }
+        return entityManager;
+    }
+
+    //neste metodo criamos um stirng sql para fazer a pesquisa por "nome" no banco de dads
+    public List<Tarefa> pesquisarNome(String nome) {
+        entityManager.getTransaction().begin();
+
+        String hql = "select t from Tarefas t where t.nome = :nome";
+
+        Query query = entityManager.createQuery(hql);
+
+        query.setParameter("nome", nome);  // define que o parametro para pesquisa e o nome
+
+        List<Tarefa> nomeLista = query.getResultList();
+
+        entityManager.getTransaction().commit();  // commit da transacao sempre deve ter
+
+        return nomeLista;
+    }
+
+    public void inserirTarefa(Tarefa tarefa) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(tarefa);
+            entityManager.getTransaction().commit();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
+        JOptionPane.showMessageDialog(null, "Tarefa cadastrada com Sucesso");
+    }
+
+    public void updatePessoa(Tarefa tarefa) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(tarefa); //  merge para atualizar a entidade
+            entityManager.getTransaction().commit();
+            
+            JOptionPane.showMessageDialog(null, "Dados Alterados com Sucesso");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
+    }
+
+    public void deletePessoa(int id) {
+        try {
+            entityManager.getTransaction().begin();
+            Tarefa pessoa = entityManager.find(Tarefa.class, id);
+            if (pessoa != null) {
+                entityManager.remove(pessoa);
+            }
+            JOptionPane.showMessageDialog(null, "Deletado com Sucesso");
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
+    }
+
 }
