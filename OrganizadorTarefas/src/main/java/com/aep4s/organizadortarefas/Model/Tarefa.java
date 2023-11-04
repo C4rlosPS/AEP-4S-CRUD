815bc791051b93d1b842547fbf7065e787222f0d
Carlos Eduardo <cadu.ps04@gmail.com>
2023-11-04 09:55:17 -0300

    Class Tarefa Criada

---
+++ OrganizadorTarefas/src/main/java/com/aep4s/organizadortarefas/Model/Tarefa.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.aep4s.organizadortarefas.Model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Usuário
+ */
+@Entity
+public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "titulo")
+    private String titulo;
+    @Column(name = "descricao")
+    private String descricao;
+    @Column(name = "criada")
+    private Date datacriada;
+    @Column(name = "vencimento")
+    private Date vence;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDatacriada() {
+        return datacriada;
+    }
+
+    public void setDatacriada(Date datacriada) {
+        this.datacriada = datacriada;
+    }
+
+    public Date getVence() {
+        return vence;
+    }
+
+    public void setVence(Date vence) {
+        this.vence = vence;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{/n" + "titulo = " + titulo + ", /ndescricao = " + descricao + ", /ndatacriada=" + datacriada + ",/nvence = " + vence + "/n}";
+    }
+    
+    
+    
+            
+}
